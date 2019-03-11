3659c5af46b858f6c9c5b319c665a6a0a601e379
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-11 16:42:32 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,3 @@
 # Build your say_hello method here
-def say_hello(name)
-  if name == "John"
-    puts "Hello #{name}"
-  else 
-    puts "Hello Ruby Programmer!"
-  end
-end
+def say_hello("John")
+  puts "Hello John"
